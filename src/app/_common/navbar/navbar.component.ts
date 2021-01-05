333539a5753f16461d4b5ee1478788170e858ff7
karthik1580 <karthikp1580@gmail.com>
2021-01-06 01:13:48 +0530

    navbar component update

--- src/app/_common/navbar/navbar.component.ts
+++ src/app/_common/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { paths } from '../../models/paths';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  paths = paths;
   constructor() { }
 
   ngOnInit(): void {
