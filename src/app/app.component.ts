398deda89a691f36dcc8717f08a54bc811e7630c
karthik1580 <karthikp1580@gmail.com>
2021-01-06 19:37:44 +0530

    adding Router start, end, cancel, error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
-import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import { 
+  Router, 
+  Event as RouterEvent, 
+  NavigationStart, 
+  NavigationEnd, 
+  NavigationError, 
+  NavigationCancel 
+} from '@angular/router';
 // import * as AOS from 'aos';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +18,35 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 })
 export class AppComponent {
   title = 'reactivform-app';
+  public spinnerLoading = true;
+
   constructor(
     private _loading: NgCircleProgressModule, 
     private _router: Router){
-      this._router.events.subscribe((event: Event) => {
+      this._router.events
+      .pipe(filter((event: any) => event instanceof NavigationStart || event instanceof NavigationEnd))
+      .subscribe((event: RouterEvent) => {
         this.navigationInterceptor(event);
       });
     }
 
-    private navigationInterceptor(event: Event): void {
-      if(Event instanceof NavigationStart){}
-      if(Event instanceof NavigationEnd){}
-      if(Event instanceof NavigationError){}
+    private navigationInterceptor(event: RouterEvent): void {
+      
+      if(event instanceof NavigationStart){
+        this.spinnerLoading = true;
+        console.log('NavigationStart');
+      }
+      if(event instanceof NavigationEnd){
+        this.spinnerLoading = false;
+        console.log('NavigationEnd');
+      }
+      if(event instanceof NavigationCancel){
+        this.spinnerLoading = false;
+        console.log('NavigationCancel');
+      }
+      if(event instanceof NavigationError){
+        this.spinnerLoading = false;
+        console.log('NavigationError');
+      }
     }
 }
